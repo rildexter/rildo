1afb459572b78524496765d938222dd2bca38e7f
rildexter <rildexter@hotmail.com>
2020-03-13 12:31:44 -0300

    dart

---
+++ teste.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  int num1 = 1;
+  int num2 = 2;
+  int soma = 0;
+  stdout.write("Entre com primeiro numero: ");
+  var nop = stdin.length  ;
+  
+  
+print(nop);
+}
